c2b9983a656953dd72276be4fa4b376a1b32c251
swathisethuram <swathi.sethuram@spirent.com>
2021-08-02 12:11:36 +0530

    Simple Interest Calculator Enhancement

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if (Number(principal) <=0){
+        alert("Enter a postivie number");
+        form.fieldname.focus();
+        return false;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = parseFloat(principal * years * rate / 100)
+    var year = new Date().getFullYear()+parseInt(years)
+    var amount = Number(principal) + Number(interest)
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+}
+
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
